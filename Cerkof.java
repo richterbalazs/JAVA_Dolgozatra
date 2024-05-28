0fb8c9f53622cfa842bfad57dd04b176fcdabcdf
richterbalazs <richter.balazs.krisztian@gmail.com>
2024-05-28 14:49:56 +0200

    dolgozatra

---
+++ Cerkof.java
@@ -0,0 +1,40 @@
+class CerkófMajom{
+
+	private static final int VODOR_DB = 3;
+
+	private boolean mocsári;
+	private Zsiraf[] zsirafok
+	private int zsirafDb;
+
+	public CerkófMajom(){
+		this(true);
+	}
+	
+	public CerkófMajom(boolean mocsári){
+		this.mocsári = mocsari;
+		zsirafok = new Zsiraf[5];
+		zsirafDb = 0; 
+	}
+	
+	public ismerkedik(Zsiraf zsiraf){
+		if(zsirafDb < 5) {
+			zsirafok[zsirafDb++] = zsiraf;
+		}
+	}
+	
+	private void bábjátékozik(){
+		
+	}
+	
+	public double énekel(String dal){
+		return 0.0;
+	}
+	
+	public void szemuveg(Kartya[] csomag){
+		
+	}
+	
+	public List<Létra> dal(asd[] géza){
+		return new List<Létra>();
+	}
+}
